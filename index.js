241a8ed2b848a4495184d7c0985eb4fc83c4819c
Utsav Jain <utsavjain1408@gmail.com>
2018-01-03 23:49:42 -0800

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+import nodemon from 'nodemon'
+import bodyParser from 'body-parser'
+
+const PORT = 3000
+
+const app = express()
+
+app.listen(PORT, () =>console.log(`Server is running at ${PORT}`))
+
+app.get('/', (req,res) =>{
+    
+    res.sendFile('home.html',{"root": __dirname})
+})
+//console.log(`Server running at 3000`);
